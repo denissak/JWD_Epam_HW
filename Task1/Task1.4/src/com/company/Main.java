6e3a9034559eb82cd92cc12a1a2c11d16e2b9c6f
Denis Sak <denissak9@gmail.com>
2021-05-17 09:07:00 +0300

    fix 16.05 task1

--- Task1/Task1.4/src/com/company/Main.java
+++ Task1/Task1.4/src/com/company/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число A");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         int numbA = scanner.nextInt();
         System.out.println("Введите число B");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         int numbB = scanner.nextInt();
         System.out.println("Введите число C");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         int numbC = scanner.nextInt();
         System.out.println("Введите число D");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         int numbD = scanner.nextInt();
         EvenNumbers evenNumbers = new EvenNumbers();
         evenNumbers.compare(numbA, numbB, numbC, numbD);
     }
 
-    static void checkError(String message, Scanner scanner) {
+    static void checkError(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             System.err.println("Вы ввели не число. Повторите");
             scanner.next();
--- Task1/Task1.7/src/com/company/Main.java
+++ Task1/Task1.7/src/com/company/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координату x1");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         double x1 = scanner.nextInt();
         System.out.println("Введите координату y1");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         double y1 = scanner.nextInt();
         System.out.println("Введите координату x2");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         double x2 = scanner.nextInt();
         System.out.println("Введите координату y2");
-        checkError("Вы ввели не число. Повторите", scanner);
+        checkError(scanner);
         double y2 = scanner.nextInt();
         CompareDots dots = new CompareDots();
         dots.Compare(x1, y1, x2, y2);
     }
 
-    static void checkError(String message, Scanner scanner) {
+    static void checkError(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             System.err.println("Вы ввели не число. Повторите");
             scanner.next();
