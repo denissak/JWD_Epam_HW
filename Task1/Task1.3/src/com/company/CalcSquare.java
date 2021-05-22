819e3cb08cdf0b214c3ab5c1c058a5dd14947093
denissak <56992508+denissak@users.noreply.github.com>
2021-05-22 23:51:54 +0300

    Delete Task1 directory

--- Task1/Task1.3/src/com/company/CalcSquare.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class CalcSquare {
-
-    void calcSquare(double squareOuter) {
-        System.out.println("Площадь вписанного квадрата будет равна " + squareOuter / 2);
-    }
-}
