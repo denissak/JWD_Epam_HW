d40e594d91c5724508e2fa3244b58f55c75c8d1e
denissak <56992508+denissak@users.noreply.github.com>
2021-05-23 09:23:38 +0300

    Delete Task1 directory

--- Task1/src/by/epam/sak/homework1/task1/LastNumberSquare.java
+++
@@ -1,7 +0,0 @@
-package by.epam.sak.homework1.task1;
-
-class LastNumberSquare {
-    static int lastNumberSquare(int number) {
-        return (number % 10 * number % 10) % 10;
-    }
-}
--- Task1/src/by/epam/sak/homework1/task3/CalcSquare.java
+++
@@ -1,8 +0,0 @@
-package by.epam.sak.homework1.task3;
-
-class CalcSquare {
-
-    static double calcSquare(double squareOuter) {
-        return squareOuter / 2;
-    }
-}
